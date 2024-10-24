35c2b2201ae9b032049f4d049092f0b609928746
Dmitry Lihachev <dmitry.lihachev@apliteni.com>
2024-10-24 15:18:52 +0700

    Quote vars

--- docker-entrypoint-library.sh
+++ docker-entrypoint-library.sh
@@ -5,7 +5,7 @@ DEFAULT_LOG_LEVEL='info'
 
 print_full_var_name() {
   local var_name="${1}"
-  local upcased_container_name="$(echo ${COMPONENT_NAME} | tr '[a-z]' '[A-Z]')"
+  local upcased_container_name="$(echo "${COMPONENT_NAME:-}" | tr '[a-z]' '[A-Z]')"
   local prefix="${upcased_container_name//-/_}"
 
   if [[ "${prefix}" == "" ]]; then
